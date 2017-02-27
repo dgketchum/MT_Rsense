8a0b7d6623030bcb08188c00b07a88d23b2d5174
dgketchum <dgketchum@gmail.com>
2017-02-27 11:48:02 -0700

    still attempting to merge

--- gdal_utils/gdal_warp_rasters_subprocCall.py
+++ gdal_utils/gdal_warp_rasters_subprocCall.py
@@ -17,11 +17,9 @@
 # standard library imports ======================================================
 import os
 import gdal
+from gdalconst import*
 import osr
 from subprocess import call
-
-
-# os.environ['GDAL_DATA'] = os.popen('gdal -config --datadir').read().rstrip()
 
 
 def merge_rasters(in_folder, out_location, out_name, out_proj):
@@ -31,34 +29,28 @@ def merge_rasters(in_folder, out_location, out_name, out_proj):
     target_crs = osr.SpatialReference()
     target_crs.ImportFromEPSG(out_proj)
 
-    # print 'tif string to save: {}'.format(tif_string)
     for tif in tifs:
         dataset = gdal.Open(tif)
-        # print dataset.GetProjectionRef()
+        print 'source srs: {}'.format(dataset.GetProjection())
 
-        # print 'source srs: {}'.format(dataset.GetProjection())
+    print 'working space: {}'.format(os.getcwd())
+    os.chdir(in_folder)
+    vrt_str = 'vrt_merge_id.vrt'
 
-    # source_epsg = 4326
-    # target_epsg = 32100
-    #
-    # print 'working space: {}'.format(os.getcwd())
-    # os.chdir(in_folder)
-    # vrt_str = 'vrt_merge_id.vrt'
-    #
-    # vrts = 'gdalbuildvrt -overwrite -allow_projection_difference {} {}'.format(vrt_str, tif_string)
-    # call(vrts, shell=True)
-    #
-    # warp = 'gdalwarp -s_srs {} -t_srs {} -tr 30 -r cubic -srcnodata 0.0 dstnodata 0.0 \n' \
-    #        '{} {}'.format(source_epsg,
-    #                       target_epsg, vrt_str, os.path.join(out_location, out_name))
-    #
-    # print 'warp cmd: {}'.format(warp)
-    # call(warp, shell=True)
+    vrts = 'gdalbuildvrt -overwrite -allow_projection_difference {} {}'.format(vrt_str, tif_string)
+    call(vrts, shell=True)
+
+    warp = 'gdalwarp -s_srs {} -t_srs {} -tr 30 -r cubic -srcnodata 0.0 dstnodata 0.0 \n' \
+           '{} {}'.format(source_epsg,
+                          target_crs, vrt_str, os.path.join(out_location, out_name))
+
+    print 'warp cmd: {}'.format(warp)
+    call(warp, shell=True)
 
 
 if __name__ == '__main__':
     home = os.path.expanduser('~')
-    print 'home: {}'.format(home)
+    # print 'home: {}'.format(home)
     images = os.path.join(home, 'images')
     tiles = os.path.join(images, 'DEM', 'elevation_NED30M_id_22371_01', 'elevation')
     merge_rasters(tiles, os.path.join(images, 'DEM'), 'id_dem_full_30m_proj.tif', out_proj=32100)
