194ed394756d5f82087a687ccce7528c82e2838b
dgketchum <dgketchum@gmail.com>
2017-03-29 07:38:05 -0600

    put download landsat in function

--- utils/down_landsat.py
+++ utils/down_landsat.py
@@ -16,9 +16,61 @@ import requests.packages.urllib3
 
 requests.packages.urllib3.disable_warnings()
 
-shp_filename = 'C:/Recharge_GIS/Landsat_Paths/NM_wrs2_desc.shp'
-ds = ogr.Open(shp_filename)
-lyr = ds.GetLayer()
+
+def download_landsat((path, row)=None, (lat, lon)=None, shape):
+    shp_filename = 'C:/Recharge_GIS/Landsat_Paths/NM_wrs2_desc.shp'
+    ds = ogr.Open(shp_filename)
+    lyr = ds.GetLayer()
+
+    image_index = get_pathrow(lyr)
+
+    latitude = 32.88205
+    longitude = -105.17832
+    path = None
+    row = None
+    s_date = '2013-02-11'
+    e_date = '2016-05-14'
+    return_scenes = 100
+    max_cloud_prcnt = 20
+    data_dir = 'F:\EOS\Landsat_Images\Landsat_8'
+
+    for tile in image_index:
+        path, row = tile[0], tile[1]
+        srcher = Search()
+        dest_path = 'd_{}_{}'.format(path, row)
+        os.chdir(data_dir)
+        if not os.path.exists(dest_path):
+            os.makedirs(dest_path)
+        # if os.listdir(dest_path) == []:
+        print os.listdir(dest_path)
+        print '{} is empty'.format(dest_path)
+
+        dwner = Downloader(verbose=False, download_dir='{}\\{}'.format(data_dir, dest_path))
+
+        candidate_scenes = srcher.search(paths_rows='{},{},{},{}'.format(path, row, path, row),
+                                         start_date=s_date,
+                                         end_date=e_date,
+                                         cloud_min=0,
+                                         cloud_max=max_cloud_prcnt,
+                                         limit=return_scenes)
+        print 'total images for tile {} is {}'.format(tile, candidate_scenes['total_returned'])
+
+        x = 0
+        if candidate_scenes['status'] == 'SUCCESS':
+            for scene_image in candidate_scenes['results']:
+                print 'Downloading:', (str(scene_image['sceneID']))
+                print 'Downloading tile {} of {}'.format(x, candidate_scenes['total_returned'])
+                try:
+                    dwner.download([str(scene_image['sceneID'])])
+                    Simple(join('{}\\{}'.format(data_dir, dest_path), str(scene_image['sceneID']) + '.tar.bz'))
+                    x += 1
+                except RemoteFileDoesntExist:
+                    print 'Skipping:', (str(scene_image['sceneID']))
+
+        else:
+            print 'nothing'
+
+    print 'done'
 
 
 def get_pathrow(layer):
@@ -30,54 +82,3 @@ def get_pathrow(layer):
     print path_list
     print 'number of tiles : {}'.format(len(path_list))
     return path_list
-
-
-image_index = get_pathrow(lyr)
-
-latitude = 32.88205
-longitude = -105.17832
-path = None
-row = None
-s_date = '2013-02-11'
-e_date = '2016-05-14'
-return_scenes = 100
-max_cloud_prcnt = 20
-data_dir = 'F:\EOS\Landsat_Images\Landsat_8'
-
-for tile in image_index:
-    path, row = tile[0], tile[1]
-    srcher = Search()
-    dest_path = 'd_{}_{}'.format(path, row)
-    os.chdir(data_dir)
-    if not os.path.exists(dest_path):
-        os.makedirs(dest_path)
-    # if os.listdir(dest_path) == []:
-    print os.listdir(dest_path)
-    print '{} is empty'.format(dest_path)
-
-    dwner = Downloader(verbose=False, download_dir='{}\\{}'.format(data_dir, dest_path))
-
-    candidate_scenes = srcher.search(paths_rows='{},{},{},{}'.format(path, row, path, row),
-                                     start_date=s_date,
-                                     end_date=e_date,
-                                     cloud_min=0,
-                                     cloud_max=max_cloud_prcnt,
-                                     limit=return_scenes)
-    print 'total images for tile {} is {}'.format(tile, candidate_scenes['total_returned'])
-
-    x = 0
-    if candidate_scenes['status'] == 'SUCCESS':
-        for scene_image in candidate_scenes['results']:
-            print 'Downloading:', (str(scene_image['sceneID']))
-            print 'Downloading tile {} of {}'.format(x, candidate_scenes['total_returned'])
-            try:
-                dwner.download([str(scene_image['sceneID'])])
-                Simple(join('{}\\{}'.format(data_dir, dest_path), str(scene_image['sceneID']) + '.tar.bz'))
-                x += 1
-            except RemoteFileDoesntExist:
-                print 'Skipping:', (str(scene_image['sceneID']))
-
-    else:
-        print 'nothing'
-
-print 'done'
