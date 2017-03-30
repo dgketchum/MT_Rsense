280de2ab2310b0bc5e1f3279381aca3263bbecef
dgketchum <dgketchum@gmail.com>
2017-03-30 07:20:59 -0600

    put download landsat in function

--- utils/down_landsat.py
+++ utils/down_landsat.py
@@ -17,39 +17,41 @@ import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
 
 
-def download_landsat((path, row)=None, (lat, lon)=None, shape):
-    shp_filename = 'C:/Recharge_GIS/Landsat_Paths/NM_wrs2_desc.shp'
-    ds = ogr.Open(shp_filename)
-    lyr = ds.GetLayer()
+def download_landsat((start_date, end_date), (path, row)=None, (lat, lon)=None, shape=None, output_path=None):
+    if shape:
+        shp_filename = 'C:/Recharge_GIS/Landsat_Paths/NM_wrs2_desc.shp'
+        ds = ogr.Open(shp_filename)
+        lyr = ds.GetLayer()
+        image_index = get_pathrow(lyr)
+    elif (lat, lon):
+        latitude = 32.88205
+        longitude = -105.17832
+    elif (path, row):
+        path = None
+        row = None
+    else:
+        raise NotImplementedError('You must give path/row tuple, lat/lon tuple, or a path/row shapefile!')
 
-    image_index = get_pathrow(lyr)
-
-    latitude = 32.88205
-    longitude = -105.17832
-    path = None
-    row = None
-    s_date = '2013-02-11'
-    e_date = '2016-05-14'
     return_scenes = 100
     max_cloud_prcnt = 20
-    data_dir = 'F:\EOS\Landsat_Images\Landsat_8'
 
     for tile in image_index:
         path, row = tile[0], tile[1]
         srcher = Search()
         dest_path = 'd_{}_{}'.format(path, row)
-        os.chdir(data_dir)
+        os.chdir(output_path)
         if not os.path.exists(dest_path):
             os.makedirs(dest_path)
         # if os.listdir(dest_path) == []:
-        print os.listdir(dest_path)
+        if os.listdir(dest_path)
+            print
         print '{} is empty'.format(dest_path)
 
-        dwner = Downloader(verbose=False, download_dir='{}\\{}'.format(data_dir, dest_path))
+        dwner = Downloader(verbose=False, download_dir='{}\\{}'.format(output_path, dest_path))
 
         candidate_scenes = srcher.search(paths_rows='{},{},{},{}'.format(path, row, path, row),
-                                         start_date=s_date,
-                                         end_date=e_date,
+                                         start_date=start_date,
+                                         end_date=end_date,
                                          cloud_min=0,
                                          cloud_max=max_cloud_prcnt,
                                          limit=return_scenes)
@@ -62,7 +64,7 @@ def download_landsat((path, row)=None, (lat, lon)=None, shape):
                 print 'Downloading tile {} of {}'.format(x, candidate_scenes['total_returned'])
                 try:
                     dwner.download([str(scene_image['sceneID'])])
-                    Simple(join('{}\\{}'.format(data_dir, dest_path), str(scene_image['sceneID']) + '.tar.bz'))
+                    Simple(join('{}\\{}'.format(output_path, dest_path), str(scene_image['sceneID']) + '.tar.bz'))
                     x += 1
                 except RemoteFileDoesntExist:
                     print 'Skipping:', (str(scene_image['sceneID']))
