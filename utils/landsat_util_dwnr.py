f83848b77dd5198335d9b625bd8f6d5444707e49
cna372 <david.ketchum@gmail.com>
2017-04-10 15:53:35 -0600

    initial commit of olivier

--- utils/landsat_util_dwnr.py
+++ utils/landsat_util_dwnr.py
@@ -10,34 +10,37 @@ import requests.packages.urllib3
 from osgeo import ogr
 from datetime import datetime
 
-import landsat
+from landsat.search import Search
+from landsat.downloader import Downloader, RemoteFileDoesntExist
+from landsat.image import Simple
 
-from vector_tools import get_pr_from_field
+from utils import olivier
+
+from vector_tools import get_pr_from_field, get_pr_multipath
 from web_tools import lat_lon_to_path_row
 
 requests.packages.urllib3.disable_warnings()
 
 
 def download_landsat(start_end_tuple, satellite='L8', path_row_tuple=None, lat_lon_tuple=None,
-                     shape=None, output_path=None, seek_multipath=False,
+                     shape=None, output_path=None, seek_multipath=False, multipath_points=None,
                      dry_run=False, max_cloud=None, return_scenes=100):
 
     start_date, end_date = start_end_tuple[0], start_end_tuple[1]
     print 'Date range: {} to {}'.format(start_date, end_date)
 
-    if shape:
+    if shape and not seek_multipath:
         # assumes shapefile has a 'path' and a 'row' field
         ds = ogr.Open(shape)
         lyr = ds.GetLayer()
         image_index = get_pr_from_field(lyr)
-        assert type(image_index) == list
         print 'Downloading landsat by row/path shapefile: {}'.format(shape)
 
-    if seek_multipath:
     # for case with path,row shapefile and point(s) attempts to get path, row overlapping scenes
     # thereby increasing images/time
-
-
+    elif seek_multipath:
+        image_index = get_pr_multipath(multipath_points, shape)
+        print 'Downloading landsat for multipath\nshapefile: {}\npoints shapefile: {}'.format(shape, multipath_points)
 
     elif lat_lon_tuple:
         # for case of lat and lon
@@ -56,41 +59,41 @@ def download_landsat(start_end_tuple, satellite='L8', path_row_tuple=None, lat_l
 
     print 'Image Ind: {}'.format(image_index)
 
-    for tile in image_index:
-        path, row = tile[0], tile[1]
-        searcher = landsat.search.Search()
-        destination_path = os.path.join(output_path, 'd_{}_{}'.format(path, row))
-        os.chdir(output_path)
-
-        downer = landsat.downloader.Downloader(verbose=False, download_dir=destination_path)
-
-        candidate_scenes = searcher.search(paths_rows='{},{},{},{}'.format(path, row, path, row),
-                                           start_date=start_date,
-                                           end_date=end_date,
-                                           cloud_min=0,
-                                           cloud_max=max_cloud,
-                                           limit=return_scenes)
-
-        print 'total images for tile {} is {}'.format(tile, candidate_scenes['total_returned'])
-
-        x = 0
-
-        if candidate_scenes['status'] == 'SUCCESS':
-            for scene_image in candidate_scenes['results']:
-                print 'Downloading:', (str(scene_image['sceneID']))
-                if not dry_run:
-                    try:
-                        print 'Downloading tile {} of {}'.format(x, candidate_scenes['total_returned'])
-                        downer.download([str(scene_image['sceneID'])])
-                        landsat.image.Simple(
-                            os.path.join(output_path, destination_path,
-                                         '{}.tar.bz'.format(str(scene_image['sceneID']))))
-                        x += 1
-                    except landsat.downloader.RemoteFileDoesntExist:
-                        print 'Skipping:', (str(scene_image['sceneID']))
-
-        else:
-            print 'nothing'
+    # for tile in image_index:
+    #     path, row = tile[0], tile[1]
+    #     searcher = Search()
+    #     destination_path = os.path.join(output_path, 'd_{}_{}'.format(path, row))
+    #     os.chdir(output_path)
+    #
+    #     downer = Downloader(verbose=False, download_dir=destination_path)
+    #
+    #     candidate_scenes = searcher.search(paths_rows='{},{},{},{}'.format(path, row, path, row),
+    #                                        start_date=start_date,
+    #                                        end_date=end_date,
+    #                                        cloud_min=0,
+    #                                        cloud_max=max_cloud,
+    #                                        limit=return_scenes)
+    #
+    #     print 'total images for tile {} is {}'.format(tile, candidate_scenes['total_returned'])
+    #
+    #     x = 0
+    #
+    #     if candidate_scenes['status'] == 'SUCCESS':
+    #         for scene_image in candidate_scenes['results']:
+    #             print 'Downloading:', (str(scene_image['sceneID']))
+    #             if not dry_run:
+    #                 try:
+    #                     print 'Downloading tile {} of {}'.format(x, candidate_scenes['total_returned'])
+    #                     downer.download([str(scene_image['sceneID'])])
+    #                     Simple(
+    #                         os.path.join(output_path, destination_path,
+    #                                      '{}.tar.bz'.format(str(scene_image['sceneID']))))
+    #                     x += 1
+    #                 except RemoteFileDoesntExist:
+    #                     print 'Skipping:', (str(scene_image['sceneID']))
+    #
+    #     else:
+    #         print 'nothing'
 
     print 'done'
 
@@ -106,6 +109,6 @@ if __name__ == '__main__':
     lat, lon = 44.91, -106.55
     path_int, row_int = 36, 25
     download_landsat((start, end), shape=poly, seek_multipath=True, dry_run=True,
-                     output_path=output, max_cloud=70)
+                     output_path=output, max_cloud=70, multipath_points=flux_sites)
 
     # ===============================================================================
