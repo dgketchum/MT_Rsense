6840bc368de2a15336c5f18b42bb9498eec1f478
dgketchum <pdWs7kyy>
2021-10-12 21:27:16 -0600

    join pou and pod to union geometries

--- utils/water_rights.py
+++ utils/water_rights.py
@@ -1,5 +1,26 @@
-from geopandas import read_file
+import os
+import numpy as np
+import pandas as pd
+from shapely import geometry
+from geopandas import read_file, GeoDataFrame, GeoSeries
+from pandas import Series, DataFrame
 from fuzzywuzzy import process, fuzz
+
+pou_cols = ['OBJECTID', 'WRKEY', 'WRID', 'VERID', 'WRNUMBER', 'ALLOWNERS', 'WRTYPE', 'HISTRGTTYP', 'WRSTATUS',
+            'LATECLAIM', 'PRIDATE', 'ENFRPRIDAT', 'POUID', 'PURPNUMBER', 'PURPOSE', 'IRRTYPE', 'MAXACRES', 'FLWRTGPM',
+            'FLWRTCFS', 'VOL', 'ACREAGE', 'COUNTY', 'TR', 'SECNO', 'QSECTION', 'GOVTLOT', 'LLDSID', 'TRSSID',
+            'ABSTRACT', 'LAYER_DATE', 'SHAPE_Leng', 'SHAPE_Area', 'geometry']
+
+NON_UNIQUE = ['WRKEY', 'WRID', 'VERID', 'WRNUMBER', 'ALLOWNERS', 'WRTYPE', 'HISTRGTTYP', 'WRSTATUS', 'LATECLAIM',
+              'PRIDATE', 'ENFRPRIDAT', 'PURPNUMBER', 'PURPOSE', 'IRRTYPE', 'MAXACRES', 'FLWRTGPM', 'FLWRTCFS', 'VOL',
+              'COUNTY', 'GOVTLOT', 'ABSTRACT', 'LAYER_DATE']
+
+DROP_UNIQUE = ['OBJECTID', 'POUID', 'ACREAGE', 'TR', 'SECNO', 'QSECTION', 'LLDSID', 'TRSSID', 'SHAPE_Leng',
+               'SHAPE_Area', 'geometry']
+
+DROP_RESMP = ['HISTRGTTYP', 'LATECLAIM', 'IRRTYPE', 'GOVTLOT']
+
+CONV_DTYPE= ['MAXACRES', 'FLWRTGPM', 'FLWRTCFS', 'VOL']
 
 
 def ap_fuzz(x, y):
@@ -20,13 +41,32 @@ def fuzzy_join(wr_shp, cad_shp, out_name):
     df.to_file(out_name.replace('.csv', '.shp'))
 
 
-def join_pou_pod():
-    pass
+def join_pou_pod(pod_src, pou_src, pou_out):
+    pod = read_file(pod_src)
+    pod = pod[pod['SRCTYPE'] == 'SURFACE']
+    pou = read_file(pou_src)
+    pou = pou[(pou['PURPOSE'] == 'IRRIGATION') & (pou['WRSTATUS'] == 'ACTIVE')]
+    geo = [(key, pou[pou['WRKEY'] == key]['geometry'].unary_union) for key in pou['WRKEY'].unique()]
+    pou.drop(columns=DROP_UNIQUE + DROP_RESMP, inplace=True)
+    pou = pou.groupby(['WRKEY'], as_index=False).agg(Series.mode)
+
+    pou = pou.astype({c: str for c in pou.columns})
+    pou['geometry'] = [x[1] for x in geo]
+
+    addtl_drop = ['OBJECTID', 'WELLDEPTH', 'X_METERS', 'Y_METERS', 'XY_MAPPED']
+    drop_cols = [x for x in list(pod.columns) if x in list(pou.columns) and x != 'WRKEY'] + addtl_drop
+    pod.drop(columns=drop_cols, inplace=True)
+    df = pou.merge(pod, on='WRKEY', how='inner')
+    geo = df['geometry']
+    df.drop(columns=['geometry'], inplace=True)
+    gdf = GeoDataFrame(df, geometry=geo)
+    gdf.to_file(pou_out)
 
 
 if __name__ == '__main__':
-    cadestral_fields = '/media/research/IrrigationGIS/Montana/water_rights/clark_fork_sid_32100_ownerparcel.shp'
-    w_rights = '/media/research/IrrigationGIS/Montana/water_rights/irrRights_CFR_12334550.shp'
-    out = '/media/research/IrrigationGIS/Montana/water_rights/cad_wr_merge_token_score.csv'
-    fuzzy_join(w_rights, cadestral_fields, out)
+    root = '/media/research/IrrigationGIS/Montana/water_rights/mt_wr'
+    pod_ = os.path.join(root, 'cfr_div.shp')
+    pou_ = os.path.join(root, 'cfr_pou.shp')
+    out = os.path.join(root, 'cfr_pou_surfIrr.shp')
+    join_pou_pod(pod_, pou_, out)
 # ========================= EOF ====================================================================
