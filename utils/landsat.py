03a44539b395de54afcdad50119baf2a79a9871f
dgketchum <pdWs7kyy>
2021-10-20 13:08:57 -0600

    write etf tif with rasterio

--- utils/landsat.py
+++ utils/landsat.py
@@ -1,13 +1,26 @@
+import os
 import numpy as np
+
+import rasterio
 import requests
 import xarray as xr
-from dask.array import where, unique, isin, from_array, map_blocks
-from pandas import DataFrame
+import dask
+from dask.array import map_blocks
+from pandas import DataFrame, to_datetime
 
 QA_VALUES = [322.0, 324.0, 328.0, 336.0, 352.0, 368.0, 386.0, 388.0,
              392.0, 400.0, 416.0, 432.0, 480.0, 834.0, 836.0, 840.0,
              848.0, 864.0, 880.0, 898.0, 904.0, 912.0, 928.0, 944.0,
              992.0]
+
+
+def read_raster(t, d):
+    da = xr.open_rasterio(t, parse_coordinates=True,
+                          chunks={'x': 1000, 'y': 1000})
+    da = da.squeeze()
+    da = da.drop_vars('band')
+    da['time'] = to_datetime(d)
+    return da
 
 
 def qualityDecode(value):
@@ -69,8 +82,36 @@ def get_landsat_ndvi_time_series(_file, out_):
     da.rio.to_raster(out_.replace('.nc', '.tif'))
 
 
+def get_landsat_etf_time_series(tif_dir, out_tif):
+    l = [os.path.join(tif_dir, x) for x in os.listdir(tif_dir) if x.endswith('.tif')]
+    d = [x.split('.')[0][-8:] for x in l]
+    dstr = ['{}-{}-{}'.format(x[:4], x[4:6], x[-2:]) for x in d]
+    srt = sorted([(x, y) for x, y in zip(l, dstr)], key=lambda x: x[1])
+    desc = []
+    da = None
+    first = True
+    for tif, date in srt:
+        if first:
+            with rasterio.open(tif, 'r') as src:
+                meta = src.meta
+            da = read_raster(tif, date)
+            first = False
+        else:
+            d = read_raster(tif, date)
+            da = xr.concat([da, d], dim='time')
+        desc.append(date)
+
+    meta.update(count=da.time.shape[0])
+    meta.update(nodata=0)
+    with rasterio.open(out_tif, 'w', **meta) as dst:
+        dst.descrtiptions = tuple(desc)
+        for b in range(da.time.shape[0]):
+            dst.write_band(b + 1, da[b, :, :].values)
+
+
+
 if __name__ == '__main__':
-    nc = '/media/research/IrrigationGIS/Montana/water_rights/landsat/nc/CU_LC08.001_30m_aid0001.nc'
-    nc_out = '/media/research/IrrigationGIS/Montana/water_rights/landsat/nc/ndvi_2015.nc'
-    get_landsat_ndvi_time_series(nc, nc_out)
+    tif = '/media/research/IrrigationGIS/Montana/water_rights/landsat/etf'
+    tif_o = os.path.join(tif, 'merged', 'concat.tif')
+    get_landsat_etf_time_series(tif, tif_o)
 # ========================= EOF ====================================================================
