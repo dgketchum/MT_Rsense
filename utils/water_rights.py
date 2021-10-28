ab9c60fd6d66b861d6c6102dbd4bf7ac6070979c
dgketchum <pdWs7kyy>
2021-10-28 11:55:38 -0600

    clip multiple pou to basins

--- utils/water_rights.py
+++ utils/water_rights.py
@@ -1,13 +1,12 @@
 import os
-from copy import copy
+from subprocess import check_call
 from tqdm import tqdm
-from time import sleep
-import numpy as np
-# from fuzzywuzzy import process, fuzz
-from geopandas import read_file, GeoDataFrame, array
-from shapely.ops import unary_union
+from geopandas import read_file, GeoDataFrame
 from shapely.geometry import Polygon, MultiPolygon
-from pandas import Series, to_datetime, Timestamp
+from pandas import to_datetime, Timestamp
+
+OGR = '/home/dgketchum/miniconda3/envs/metric/bin/ogr2ogr'
+OGRINFO = '/home/dgketchum/miniconda3/envs/metric/bin/ogrinfo'
 
 DROP_UNIQUE = ['OBJECTID', 'POUID', 'ACREAGE', 'TR', 'SECNO', 'QSECTION', 'LLDSID', 'TRSSID', 'SHAPE_Leng',
                'SHAPE_Area', 'geometry']
@@ -15,44 +14,18 @@ DROP_UNIQUE = ['OBJECTID', 'POUID', 'ACREAGE', 'TR', 'SECNO', 'QSECTION', 'LLDSI
 DROP_RESMP = ['HISTRGTTYP', 'LATECLAIM', 'IRRTYPE', 'GOVTLOT']
 
 
-# def ap_fuzz(x, y):
-#     pext = process.extract(x, y, limit=1, scorer=fuzz.token_sort_ratio)[0]
-#     _str, score = pext[0], pext[1]
-#     return [_str, score]
-
-
-# def fuzzy_join(wr_shp, cad_shp, out_name):
-#     cad = read_file(cad_shp)
-#     cad = cad[~cad['OwnerName'].isnull()]
-#     wr_shp = read_file(wr_shp).drop(columns='geometry')
-#     cad['key'] = cad.OwnerName.apply(lambda x: ap_fuzz(x, wr_shp.ALLOWNERS))
-#     cad['fuzz_score'] = cad.key.apply(lambda x: x[1])
-#     cad['key'] = cad.key.apply(lambda x: x[0])
-#     df = cad.merge(wr_shp, left_on='key', right_on='ALLOWNERS')
-#     df.to_csv(out_name)
-#     df.to_file(out_name.replace('.csv', '.shp'))
-
-
-def join_unary_pou_pod(pod_src, pou_src, pou_out):
-    pod = read_file(pod_src)
-    pod = pod[pod['SRCTYPE'] == 'SURFACE']
-    pou = read_file(pou_src)
-    pou = pou[(pou['PURPOSE'] == 'IRRIGATION') & (pou['WRSTATUS'] == 'ACTIVE')]
-    geo = [(key, pou[pou['WRKEY'] == key]['geometry'].unary_union) for key in pou['WRKEY'].unique()]
-    pou.drop(columns=DROP_UNIQUE + DROP_RESMP, inplace=True)
-    pou = pou.groupby(['WRKEY'], as_index=False).agg(Series.mode)
-
-    pou = pou.astype({c: str for c in pou.columns})
-    pou['geometry'] = [x[1] for x in geo]
-
-    addtl_drop = ['OBJECTID', 'WELLDEPTH', 'X_METERS', 'Y_METERS', 'XY_MAPPED']
-    drop_cols = [x for x in list(pod.columns) if x in list(pou.columns) and x != 'WRKEY'] + addtl_drop
-    pod.drop(columns=drop_cols, inplace=True)
-    df = pou.merge(pod, on='WRKEY', how='inner')
-    geo = df['geometry']
-    df.drop(columns=['geometry'], inplace=True)
-    gdf = GeoDataFrame(df, geometry=geo)
-    gdf.to_file(pou_out)
+def clip_pou_to_basin(gdb, basin_shp_dir, pou_dir):
+    shapes = [os.path.join(basin_shp_dir, x) for x in os.listdir(basin_shp_dir) if x.endswith('.shp')]
+    cmd = [OGRINFO, '--config', '-sql', "CREATE SPATIAL INDEX ON {}".format(gdb.strip('.shp')), gdb]
+    check_call(cmd)
+    for s in shapes:
+        splt = os.path.basename(s).replace('.shp', '_pou.shp')
+        o = os.path.join(pou_dir,  splt)
+        cmd = [OGRINFO, '--config', '-sql', "CREATE SPATIAL INDEX ON {}".format(s.strip('.shp')), s]
+        check_call(cmd)
+        cmd = [OGR, '-progress', '-f', 'ESRI Shapefile', '-clipsrc', s, o, gdb]
+        print(' '.join(cmd))
+        check_call(cmd)
 
 
 def get_flat_priority(pou_src, out_file):
@@ -101,8 +74,10 @@ def get_flat_priority(pou_src, out_file):
 
 
 if __name__ == '__main__':
-    root = '/media/research/IrrigationGIS/Montana/water_rights/mt_wr'
-    pou_ = os.path.join(root, 'cfr_surf_irr.shp')
-    out = os.path.join(root, 'cfr_pou_flat_dates.shp')
-    get_flat_priority(pou_, out)
+    root = '/media/research/IrrigationGIS/Montana/water_rights'
+    shapes = os.path.join(root, 'gage_basins')
+    pou_d = os.path.join(root, 'pou')
+    gdb_ = os.path.join(root, 'wr_gdb', 'wrpou.shp')
+    clip_pou_to_basin(gdb_, basin_shp_dir=shapes, pou_dir=pou_d)
+
 # ========================= EOF ====================================================================
