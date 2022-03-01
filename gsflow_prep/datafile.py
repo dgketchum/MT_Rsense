337f417632e5360b7667240637abe3cf5d15a963
dgketchum <pdWs7kyy>
2022-03-01 10:08:02 -0700

    write PRMS data file

--- gsflow_prep/datafile.py
+++ gsflow_prep/datafile.py
@@ -1,21 +1,21 @@
 import os
-from ftplib import FTP
-from collections import OrderedDict
+from datetime import datetime
 
-import numpy as np
-from pandas import DataFrame, read_csv, date_range, concat
+from pandas import read_csv, date_range, to_datetime, isna, DataFrame
 import fiona
-from shapely.geometry import Point, mapping, shape
+from shapely.geometry import shape
 
-from utils.hydrograph import get_station_daterange_data
+from utils.hydrograph import get_station_daily_data
 from utils.elevation import elevation_from_coordinate
 
 
-def write_basin_datafile(basin_shp, ghcn_shp, snotel_shp, ghcn_data, gages_shp, n_ghcn=None):
+def write_basin_datafile(basin_shp, ghcn_shp, snotel_shp, ghcn_data, gages_shp, data_file, start='1990-01-01'):
     _crs = [fiona.open(shp).meta['crs'] for shp in [basin_shp, ghcn_shp, snotel_shp]]
     assert all(x == _crs[0] for x in _crs)
     with fiona.open(basin_shp, 'r') as basn:
         basin_geo = shape([f['geometry'] for f in basn][0])
+
+    dt_index = date_range(start, '2021-12-31')
 
     snotel_stations, snotel_names = [], []
     with fiona.open(snotel_shp, 'r') as sntstn:
@@ -26,20 +26,6 @@ def write_basin_datafile(basin_shp, ghcn_shp, snotel_shp, ghcn_data, gages_shp, 
             snotel_stations.append(f['properties']['ID'])
             snotel_names.append(f['properties']['Name'])
 
-    gage_dct = {}
-    with fiona.open(gages_shp, 'r') as src:
-        for f in src:
-            geo = shape(f['geometry'])
-            if not geo.intersects(basin_geo):
-                continue
-            sta = f['properties']['STAID']
-            elev = elevation_from_coordinate(f['properties']['LAT'], f['properties']['LON'])
-            gage_dct[sta] = {'length': len(date_range(f['properties']['start'],
-                                                      f['properties']['end'])),
-                             'lat': f['properties']['LAT'],
-                             'lon': f['properties']['LON'],
-                             'elev': elev}
-
     ghcn_dct = {}
     with fiona.open(ghcn_shp, 'r') as ghcnstn:
         for f in ghcnstn:
@@ -49,39 +35,130 @@ def write_basin_datafile(basin_shp, ghcn_shp, snotel_shp, ghcn_data, gages_shp, 
             if not geo.intersects(basin_geo):
                 continue
 
-            if f['properties']['STANAME'] in snotel_names:
+            name = f['properties']['STANAME']
+            if name in snotel_names:
                 snotel = True
             else:
                 snotel = False
 
-            ghcn_dct[f['properties']['STAID']] = {'length': len(date_range(f['properties']['START'],
+            ghcn_dct[f['properties']['STAID']] = {'start': f['properties']['START'],
+                                                  'end': f['properties']['END'],
+                                                  'length': len(date_range(f['properties']['START'],
                                                                            f['properties']['END'])),
                                                   'lat': f['properties']['LAT'],
                                                   'lon': f['properties']['LON'],
                                                   'elev': f['properties']['ELEV'],
-                                                  'snotel': snotel}
+                                                  'snotel': snotel,
+                                                  'name': name}
 
     ghcn_srt = [k for k, v in sorted(ghcn_dct.items(), key=lambda x: x[1]['length'], reverse=True)]
-
-    if n_ghcn:
-        ghcn_srt = ghcn_srt[:n_ghcn]
 
     met_dct, invalid_stations = {}, 0
     for sta in ghcn_srt:
         _file = os.path.join(ghcn_data, '{}.csv'.format(sta))
         df = read_csv(_file, parse_dates=True, infer_datetime_format=True)
-        df.index = df['DATE']
+        df.index = to_datetime(df['DATE'])
+
+        s = ghcn_dct[sta]['start']
+        if to_datetime(start) > to_datetime(s):
+            df = df.loc[start:]
+            if df.empty or df.shape[0] < 1000:
+                print(sta, 'insuf records in date range')
+                invalid_stations += 1
+                continue
+
+        df = df.reindex(dt_index)
+
         try:
-            df['TMAX'] = (df['TMAX'] / 10. * 9 / 5) + 32
-            df['TMIN'] = (df['TMIN'] / 10. * 9 / 5) + 32
-            df['PRCP'] = df['PRCP'] / 10.
             df = df[['TMAX', 'TMIN', 'PRCP']]
-        except KeyError:
+            df['tmax'] = (df['TMAX'] / 10. * 9 / 5) + 32
+            df['tmin'] = (df['TMIN'] / 10. * 9 / 5) + 32
+            df['prcp'] = df['PRCP'] / 10.
+            df = df[['tmax', 'tmin', 'prcp']]
+
+            if df.empty or df.shape[0] < 1000:
+                print(sta, 'insuf records in date range')
+                invalid_stations += 1
+
+        except KeyError as e:
+            print(sta, 'incomplete', e)
             invalid_stations += 1
             continue
+
+        df[isna(df)] = -999
+
         print(sta, df.shape[0])
         met_dct[sta] = {'data': df, 'lat': ghcn_dct[sta]['lat'], 'lon': ghcn_dct[sta]['lon'],
-                        'snotel': ghcn_dct[sta]['snotel'], 'elev': ghcn_dct[sta]['elev']}
+                        'snotel': ghcn_dct[sta]['snotel'], 'elev': ghcn_dct[sta]['elev'],
+                        'name': ghcn_dct[sta]['name']}
+
+    gage_dct = {}
+    with fiona.open(gages_shp, 'r') as src:
+        for f in src:
+            geo = shape(f['geometry'])
+            if not geo.intersects(basin_geo):
+                continue
+            sta = f['properties']['STAID']
+            s, e = f['properties']['start'], f['properties']['end']
+            elev = elevation_from_coordinate(f['properties']['LAT'], f['properties']['LON'])
+            df = get_station_daily_data('discharge', s, e, sta, freq='dv')
+            df.columns = ['flow']
+
+            if to_datetime(start) > to_datetime(s):
+                df = df.loc[start:]
+
+            df = df.tz_convert(None)
+            df = df.reindex(dt_index)
+            df[isna(df)] = -999
+
+            gage_dct[sta] = {'data': df,
+                             'lat': f['properties']['LAT'],
+                             'lon': f['properties']['LON'],
+                             'elev': elev,
+                             'name': f['properties']['STANAME'],
+                             'snotel': False}
+
+    input_dct = {**met_dct, **gage_dct}
+    dt_now = datetime.now().strftime('%Y-%m-%d %H:%M')
+
+    with open(data_file, 'w') as f:
+
+        df = DataFrame(index=dt_index)
+        df['Year'], df['Month'] = [i.year for i in df.index], [i.month for i in df.index]
+        df['day'], df['hr'], df['min'] = [i.day for i in df.index], [0 for _ in df.index], [0 for _ in df.index]
+
+        [f.write('{}\n'.format(item)) for item in ['PRMS Datafile',
+                                                   dt_now,
+                                                   ''.join(['/' for _ in range(95)]),
+                                                   '    '.join(
+                                                       ['# ID  ',
+                                                        '    Site Name{}'.format(' '.join([' ' for _ in range(14)])),
+                                                        'Type',
+                                                        'Lat',
+                                                        'Lon',
+                                                        'Elev (m)',
+                                                        'Units'])]]
+
+        counts = {'tmax': 0, 'tmin': 0, 'prcp': 0, 'flow': 0}
+        for var, unit in zip(['tmax', 'tmin', 'prcp', 'flow'], ['C', 'C', 'mm', 'cfs']):
+            for k, v in input_dct.items():
+                d = v['data']
+                if var in d.columns:
+                    line_ = ' '.join(['#', k.rjust(11, '0'),
+                                      v['name'].ljust(40, ' ')[:40],
+                                      var,
+                                      '{:.3f}'.format(v['lat']),
+                                      '{:.3f}'.format(v['lon']),
+                                      '{:.1f}'.format(v['elev']),
+                                      unit]) + '\n'
+                    f.write(line_)
+                    df['{}_{}'.format(k, var)] = d[var]
+                    counts[var] += 1
+        f.write(''.join(['/' for _ in range(95)]) + '\n')
+        for k, v in counts.items():
+            f.write('{} {}\n'.format(k, v))
+        f.write(''.join(['#' for _ in range(95)]) + '\n')
+        df.to_csv(f, sep=' ', header=False, index=False, float_format='%.2f')
 
 
 if __name__ == '__main__':
@@ -99,6 +176,7 @@ if __name__ == '__main__':
     _ghcn_data = os.path.join(clim, 'ghcn', 'ghcn_daily_summaries_4FEB2022')
     _snotel_data = os.path.join(clim, 'snotel', 'snotel_records')
     gage_shp_ = os.path.join(d, 'gages', 'gage_loc_usgs', 'selected_gages_aea.shp')
+    datafile = os.path.join(uy, 'uy.data')
 
-    write_basin_datafile(basin_, ghcn_shp_aea, snotel_shp_, _ghcn_data, gage_shp_)
+    write_basin_datafile(basin_, ghcn_shp_aea, snotel_shp_, _ghcn_data, gage_shp_, datafile)
 # ========================= EOF ====================================================================
