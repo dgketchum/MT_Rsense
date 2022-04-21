427cbed4954334b767a2c90cdfe65e00cef19833
dgketchum <pdWs7kyy>
2022-04-21 16:13:50 -0600

    update thredds call to aggregated nc file url

--- utils/thredds.py
+++ utils/thredds.py
@@ -1,12 +1,11 @@
-from __future__ import print_function, absolute_import
-
-from future.standard_library import hooks
-
 import os
 import copy
+import warnings
 from shutil import rmtree
 from tempfile import mkdtemp
 from datetime import datetime
+from urllib.parse import urlunparse
+
 from numpy import empty, float32, datetime64, timedelta64, argmin, abs, array
 from rasterio import open as rasopen
 from rasterio.crs import CRS
@@ -14,24 +13,19 @@ from rasterio.transform import Affine
 from rasterio.mask import mask
 from rasterio.warp import reproject, Resampling
 from rasterio.warp import calculate_default_transform as cdt
-from xlrd.xldate import xldate_from_date_tuple
-from xarray import open_dataset
+from xarray import open_dataset, open_mfdataset
 from pandas import date_range, DataFrame
 
-from bounds import GeoBounds
-
-with hooks():
-    from urllib.parse import urlunparse
+warnings.simplefilter(action='ignore', category=DeprecationWarning)
 
 
-class Thredds(object):
+class Thredds:
     """  Unidata's Thematic Real-time Environmental Distributed Data Services (THREDDS)
     
     """
 
     def __init__(self, start=None, end=None, date=None,
-                 bounds=None, target_profile=None, lat=None, lon=None
-                 ):
+                 bounds=None, target_profile=None, lat=None, lon=None, clip_feature=None):
         self.start = start
         self.end = end
         self.date = date
@@ -42,13 +36,16 @@ class Thredds(object):
         self.bbox = bounds
         self.lat = lat
         self.lon = lon
+        self.clip_feature = clip_feature
+        self._is_masked = False
 
     def conform(self, subset, out_file=None):
         if subset.dtype != float32:
             subset = array(subset, dtype=float32)
         self._project(subset)
-        self._reproject()
-        self._mask()
+        self._warp()
+        if self.clip_feature:
+            self._mask()
         result = self._resample()
         if out_file:
             self.save_raster(result, self.target_profile, output_filename=out_file)
@@ -79,12 +76,13 @@ class Thredds(object):
         profile.update({'crs': CRS({'init': 'epsg:4326'}),
                         'transform': dst_affine,
                         'width': dst_width,
-                        'height': dst_height})
+                        'height': dst_height,
+                        'count': subset.shape[0]})
 
         with rasopen(proj_path, 'w', **profile) as dst:
             dst.write(subset)
 
-    def _reproject(self):
+    def _warp(self):
 
         reproj_path = os.path.join(self.temp_dir, 'reproj.tif')
         setattr(self, 'reprojection', reproj_path)
@@ -92,7 +90,7 @@ class Thredds(object):
         with rasopen(self.projection, 'r') as src:
             src_profile = src.profile
             src_bounds = src.bounds
-            src_array = src.read(1)
+            src_array = src.read()
 
         dst_profile = copy.deepcopy(self.target_profile)
         dst_profile['dtype'] = float32
@@ -106,36 +104,38 @@ class Thredds(object):
         dst_profile.update({'crs': dst_profile['crs'],
                             'transform': dst_affine,
                             'width': dst_width,
-                            'height': dst_height})
+                            'height': dst_height,
+                            'count': src_array.shape[0]})
 
         with rasopen(reproj_path, 'w', **dst_profile) as dst:
-            dst_array = empty((1, dst_height, dst_width), dtype=float32)
+            dst_array = empty((src_array.shape[0], dst_height, dst_width), dtype=float32)
 
             reproject(src_array, dst_array, src_transform=src_profile['transform'],
                       src_crs=src_profile['crs'], dst_crs=self.target_profile['crs'],
                       dst_transform=dst_affine, resampling=Resampling.nearest,
                       num_threads=2)
 
-            dst.write(dst_array.reshape(1, dst_array.shape[1], dst_array.shape[2]))
+            dst.write(dst_array.reshape(src_array.shape[0], dst_array.shape[1], dst_array.shape[2]))
 
     def _mask(self):
 
         mask_path = os.path.join(self.temp_dir, 'masked.tif')
-
         with rasopen(self.reprojection) as src:
             out_arr, out_trans = mask(src, self.clip_feature, crop=True,
                                       all_touched=True)
-            out_meta = src.raster_meta.copy()
+            out_meta = src.meta.copy()
             out_meta.update({'driver': 'GTiff',
                              'height': out_arr.shape[1],
                              'width': out_arr.shape[2],
-                             'transform': out_trans})
+                             'transform': out_trans,
+                             'count': out_arr.shape[0]})
 
         with rasopen(mask_path, 'w', **out_meta) as dst:
             dst.write(out_arr)
 
+        self._is_masked = True
+
         setattr(self, 'mask', mask_path)
-        delattr(self, 'reprojection')
 
     def _resample(self):
 
@@ -144,8 +144,13 @@ class Thredds(object):
 
         resample_path = os.path.join(self.temp_dir, 'resample.tif')
 
-        with rasopen(self.mask, 'r') as src:
-            array = src.read(1)
+        if self._is_masked:
+            ras_obj = self.mask
+        else:
+            ras_obj = self.reprojection
+
+        with rasopen(ras_obj, 'r') as src:
+            array = src.read()
             profile = src.profile
             res = src.res
             try:
@@ -155,17 +160,22 @@ class Thredds(object):
             target_res = target_affine.a
             res_coeff = res[0] / target_res
 
-            new_array = empty(shape=(1, round(array.shape[0] * res_coeff),
-                                     round(array.shape[1] * res_coeff)), dtype=float32)
-            aff = src.affine
+            new_array = empty(shape=(array.shape[0], round(array.shape[1] * res_coeff),
+                                     round(array.shape[2] * res_coeff)), dtype=float32)
+            aff = src.transform
             new_affine = Affine(aff.a / res_coeff, aff.b, aff.c, aff.d, aff.e / res_coeff, aff.f)
 
-            profile['transform'] = self.target_profile['transform']
-            profile['width'] = self.target_profile['width']
-            profile['height'] = self.target_profile['height']
-            profile['dtype'] = str(new_array.dtype)
+            profile.update({'transform': self.target_profile['transform'],
+                            'width': self.target_profile['width'],
+                            'height': self.target_profile['height'],
+                            'dtype': str(new_array.dtype),
+                            'count': new_array.shape[0]})
 
-            delattr(self, 'mask')
+            try:
+                delattr(self, 'mask')
+            except AttributeError:
+                pass
+            delattr(self, 'reprojection')
 
             with rasopen(resample_path, 'w', **profile) as dst:
                 reproject(array, new_array, src_transform=aff, dst_transform=new_affine, src_crs=src.crs,
@@ -330,11 +340,11 @@ class GridMet(Thredds):
         - 'tmmx' : daily maximum air temperature [K]
         - 'vs' : daily mean wind speed [m -s]
 
-    :param start: datetime object start of period of data
-    :param end: datetime object end of period of data
+    :param start: start of period of data, datetime.datetime object or string format 'YYY-MM-DD'
+    :param end: end of period of data, datetime.datetime object or string format 'YYY-MM-DD'
     :param variables: List  of available variables. At lease one.
-    :param date: single-day datetime date object
-    :param bbox: met.misc.BBox object representing spatial bounds, default to conterminous US
+    :param date: date of data, datetime.datetime object or string format 'YYY-MM-DD'
+    :param bbox: bounds.GeoBounds object representing spatial bounds
     :return: numpy.ndarray
 
     Must have either start and end, or date.
@@ -350,10 +360,22 @@ class GridMet(Thredds):
         Thredds.__init__(self)
 
         self.date = date
+
+        if isinstance(start, str):
+            self.start = datetime.strptime(start, '%Y-%m-%d')
+        if isinstance(end, str):
+            self.end = datetime.strptime(end, '%Y-%m-%d')
+        if isinstance(date, str):
+            self.date = datetime.strptime(date, '%Y-%m-%d')
+
+        if self.start and self.end is None:
+            raise AttributeError('Must set both start and end date')
+
         self.start = start
         self.end = end
         self.bbox = bbox
         self.target_profile = target_profile
+        self.clip_feature = clip_feature
 
         self.service = 'thredds.northwestknowledge.net:8080'
         self.scheme = 'http'
@@ -369,38 +391,42 @@ class GridMet(Thredds):
             Warning('Variable {} is not available'.
                     format(self.variable))
 
-        self.kwords = {'bi': 'burning_index_g',
+        self.kwords = {'bi': 'daily_mean_burning_index_g',
                        'elev': '',
                        'erc': 'energy_release_component-g',
                        'fm100': 'dead_fuel_moisture_100hr',
                        'fm1000': 'dead_fuel_moisture_1000hr',
-                       'pdsi': 'palmer_drought_severity_index',
-                       'etr': 'potential_evapotranspiration',
-                       'pet': 'potential_evapotranspiration',
+                       'pdsi': 'daily_mean_palmer_drought_severity_index',
+                       'etr': 'daily_mean_reference_evapotranspiration_alfalfa',
+                       'pet': 'daily_mean_reference_evapotranspiration_grass',
                        'pr': 'precipitation_amount',
-                       'rmax': 'relative_humidity',
-                       'rmin': 'relative_humidity',
-                       'sph': 'specific_humidity',
-                       'srad': 'surface_downwelling_shortwave_flux_in_air',
-                       'th': 'wind_from_direction',
-                       'tmmn': 'air_temperature',
-                       'tmmx': 'air_temperature',
-                       'vs': 'wind_speed', }
+                       'rmax': 'daily_maximum_relative_humidity',
+                       'rmin': 'daily_minimum_relative_humidity',
+                       'sph': 'daily_mean_specific_humidity',
+                       'srad': 'daily_mean_shortwave_radiation_at_surface',
+                       'th': 'daily_mean_wind_direction',
+                       'tmmn': 'daily_minimum_temperature',
+                       'tmmx': 'daily_maximum_temperature',
+                       'vs': 'daily_mean_wind_speed', }
 
         if self.date:
             self.start = self.date
             self.end = self.date
 
-        self.year = self.start.year
+        if self.start.year < self.end.year:
+            self.single_year = False
+
+        if self.start > self.end:
+            raise ValueError('start date is after end date')
 
         if not self.bbox and not self.lat:
-            self.bbox = GeoBounds()
+            raise AttributeError('No bbox or coordinates given')
 
     def subset_daily_tif(self, out_filename=None):
 
         url = self._build_url()
         url = url + '#fillmismatch'
-        xray = open_dataset(url)
+        xray = open_dataset(url, decode_times=True)
 
         north_ind = argmin(abs(xray.lat.values - (self.bbox.north + 1.)))
         south_ind = argmin(abs(xray.lat.values - (self.bbox.south - 1.)))
@@ -415,24 +441,7 @@ class GridMet(Thredds):
         setattr(self, 'src_bounds_wsen', (west_val, south_val,
                                           east_val, north_val))
 
-        if self.variable != 'elev':
-            xray = xray.rename({'day': 'time'})
-            subset = xray.loc[dict(time=slice(self.start, self.end),
-                                   lat=slice(north_val, south_val),
-                                   lon=slice(west_val, east_val))]
-
-            date_ind = self._date_index()
-            subset['time'] = date_ind
-            setattr(self, 'width', subset.dims['lon'])
-            setattr(self, 'height', subset.dims['lat'])
-            arr = subset[self.kwords[self.variable]].values
-            arr = arr.reshape(arr.shape[1], arr.shape[2]).transpose()
-            arr = arr.reshape(1, arr.shape[0], arr.shape[1])
-            arr = self.conform(arr, out_file=out_filename)
-            rmtree(self.temp_dir)
-            return arr
-
-        else:
+        if self.variable == 'elev':
             subset = xray.loc[dict(lat=slice((self.bbox.north + 1),
                                              (self.bbox.south - 1)),
                                    lon=slice((self.bbox.west - 1),
@@ -441,6 +450,19 @@ class GridMet(Thredds):
             setattr(self, 'height', subset.dims['lat'])
             arr = subset.elevation.values
             arr = self.conform(arr, out_file=out_filename)
+            return arr
+
+        else:
+            xray = xray.rename({'day': 'time'})
+            subset = xray.loc[dict(time=slice(self.start, self.end),
+                                   lat=slice(north_val, south_val),
+                                   lon=slice(west_val, east_val))]
+
+            setattr(self, 'width', subset.dims['lon'])
+            setattr(self, 'height', subset.dims['lat'])
+            arr = subset[self.kwords[self.variable]].values
+            arr = self.conform(arr, out_file=out_filename)
+            rmtree(self.temp_dir)
             return arr
 
     def subset_nc(self, out_filename=None, return_array=False):
@@ -511,7 +533,7 @@ class GridMet(Thredds):
                               '', '', ''])
         else:
             url = urlunparse([self.scheme, self.service,
-                              '/thredds/dodsC/MET/{0}/{0}_{1}.nc'.format(self.variable, self.year),
+                              '/thredds/dodsC/agg_met_{}_1979_CurrentYear_CONUS.nc'.format(self.variable),
                               '', '', ''])
 
         return url
