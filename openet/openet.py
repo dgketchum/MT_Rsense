1e42febed4a73817aaf371bcbcf5367ff9c68153
dgketchum <dgketchum@gmail.com>
2022-08-08 15:42:07 -0600

    get point series from polygon shapefile (on centroids)

--- openet/openet.py
+++ openet/openet.py
@@ -5,12 +5,47 @@ from pprint import pprint
 from datetime import datetime
 
 import requests
-from pandas import read_csv, concat
+from pandas import read_csv, concat, DataFrame, date_range, to_datetime
+import geopandas as gpd
 
 requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
 with open('/home/dgketchum/PycharmProjects/MT_Rsense/data/openet_auth.json', 'r') as j:
     API_KEY = json.load(j)['auth']
+
+
+def point_series(shp, out):
+    gdf = gpd.read_file(shp)
+    start, end = '2016-01-01', '2021-12-31'
+    ct = 0
+    for i, row in gdf.iterrows():
+        file_ = os.path.join(out, str(row['FID']).rjust(3, '0'))
+        if os.path.exists(file_):
+            continue
+        lon, lat = row.geometry.centroid.xy[0], row.geometry.centroid.xy[1]
+        header = {"Authorization": API_KEY}
+        params = {'start_date': start,
+                  'end_date': end,
+                  'lon': lon, 'lat': lat,
+                  'output_file_format': 'json',
+                  'variable': 'etof',
+                  'model': 'ensemble',
+                  'ref_et_source': 'gridmet',
+                  'units': 'metric',
+                  'interval': 'monthly'}
+
+        server = 'https://openet.dri.edu/'
+        endpoint = 'raster/timeseries/point'
+        url = server + endpoint
+
+        resp = requests.get(url=url, headers=header, params=params, verify=False)
+        data = [d['etof'] for d in resp.json()]
+        time = [d['time'] for d in resp.json()]
+        time = to_datetime(time)
+        df = DataFrame(data, index=time, columns=[row['FID']])
+        df.to_csv(file_)
+        ct += 1
+        print(ct, file_)
 
 
 def monthly_ensemble(shp, year, month, filename_suffix='upper_yellowstone'):
@@ -57,15 +92,11 @@ def concatenate_openet_data(_dir, out_csv):
 
 
 if __name__ == '__main__':
-    # FIELDS = 'projects/earthengine-legacy/assets/projects/openet/field_boundaries/Upper_Yellowstone_3DEC2021'
-    # for year in range(2016, 2021):
-    #     for month in range(1, 13):
-    #         if year == 2016 and month == 1:
-    #             continue
-    #         monthly_ensemble(FIELDS, year, month, filename_suffix='uy_{}_{}'.format(year, month))
-    #         print(datetime.now())
-    d = '/home/dgketchum/Downloads/lost_creek_fan/'
-    o = os.path.join(d, 'lost_creek_fan_openet.csv')
-    r = os.path.join(d, 'openet_data')
-    concatenate_openet_data(r, o)
+    d = '/media/research/IrrigationGIS'
+    if not os.path.exists(d):
+        d = '/home/dgketchum/data/IrrigationGIS'
+    r = os.path.join(d, 'Montana', 'water_rights', 'management_factors')
+    shp_ = os.path.join(r, 'sweetgrass_fields_sample.shp')
+    out_ = os.path.join(r, 'sweetgrass_fields_etof')
+    point_series(shp_, out_)
 # ========================= EOF ====================================================================
