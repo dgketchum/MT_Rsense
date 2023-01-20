75be92852c0a7903441e7e4ee594f9931eb0c69e
dgketchum <dgketchum@gmail.com>
2023-01-19 23:05:16 -0700

    extract naip bands for water model

--- ee_api/call_ee.py
+++ ee_api/call_ee.py
@@ -1,26 +1,17 @@
 import os
 import sys
 from calendar import monthrange
-from pprint import pprint
 
-import fiona
 import numpy as np
-from pandas import read_csv
 import ee
-from matplotlib import pyplot as plt
-from scipy.stats import linregress
-
 from ee_api import is_authorized
+
+from water_availability.basin_availability import BASINS
 
 sys.path.insert(0, os.path.abspath('..'))
 sys.setrecursionlimit(5000)
 
 RF_ASSET = 'projects/ee-dgketchum/assets/IrrMapper/IrrMapperComp'
-# RF_ASSET = 'users/dgketchum/IrrMapper/IrrMapper_sw'
-BASINS = 'users/dgketchum/gages/gage_basins'
-COUNTIES = 'users/dgketchum/boundaries/western_11_co_study'
-
-FIELDS = 'users/dgketchum/boundaries/MilkHUC8'
 
 UMRB_CLIP = 'users/dgketchum/boundaries/umrb_ylstn_clip'
 CMBRB_CLIP = 'users/dgketchum/boundaries/CMB_RB_CLIP'
@@ -28,8 +19,49 @@ CORB_CLIP = 'users/dgketchum/boundaries/CO_RB'
 
 DNRC_BASINS = 'users/dgketchum/boundaries/DNRC_Basins'
 
-FLUX_SHP = '/media/research/IrrigationGIS/ameriflux/select_flux_sites/select_flux_sites_impacts_ECcorrrected.shp'
-FLUX_DIR = '/media/research/IrrigationGIS/ameriflux/ec_data/monthly'
+
+def get_geomteries():
+    bozeman = ee.Geometry.Polygon([[-111.19206055457778, 45.587493372544984],
+                                   [-110.91946228797622, 45.587493372544984],
+                                   [-110.91946228797622, 45.754947053477565],
+                                   [-111.19206055457778, 45.754947053477565],
+                                   [-111.19206055457778, 45.587493372544984]])
+
+    navajo = ee.Geometry.Polygon([[-108.50192867920967, 36.38701227276218],
+                                  [-107.92995297120186, 36.38701227276218],
+                                  [-107.92995297120186, 36.78068624960868],
+                                  [-108.50192867920967, 36.78068624960868],
+                                  [-108.50192867920967, 36.38701227276218]])
+
+    test_point = ee.Geometry.Point(-111.19206055457778, 45.587493372544984)
+
+    return bozeman, navajo, test_point
+
+
+def extract_naip(file_prefix, points_layer, roi, year):
+    roi = ee.FeatureCollection(roi)
+    naip = ee.ImageCollection("USDA/NAIP/DOQQ").filterDate('{}-01-01'.format(year), '{}-12-31'.format(year))
+    naip = naip.filterBounds(roi).mosaic()
+
+    props = ['POINT_TYPE']
+
+    points = ee.FeatureCollection(points_layer)
+    points = points.filterBounds(roi)
+
+    plot_sample_regions = naip.sampleRegions(
+        collection=points,
+        properties=props,
+        scale=1,
+        tileScale=16)
+
+    task = ee.batch.Export.table.toCloudStorage(
+        plot_sample_regions,
+        description='{}_{}'.format(file_prefix, year),
+        bucket='wudr',
+        fileNamePrefix='{}_{}'.format(file_prefix, year),
+        fileFormat='CSV')
+
+    task.start()
 
 
 def extract_terraclimate_monthly(tables, years, description):
@@ -65,13 +97,14 @@ def extract_terraclimate_monthly(tables, years, description):
             print(out_desc)
 
 
-def extract_gridded_data(tables, years=None, description=None, min_years=0, mask_swb_to_irr=True, volume=False,
-                         mask_ppt_to_irr=False):
+def export_gridded_data(tables, bucket, years, description, features=None, min_years=0, debug=False):
     """
     Reduce Regions, i.e. zonal stats: takes a statistic from a raster within the bounds of a vector.
     Use this to get e.g. irrigated area within a county, HUC, or state. This can mask based on Crop Data Layer,
     and can mask data where the sum of irrigated years is less than min_years. This will output a .csv to
     GCS wudr bucket.
+    :param features:
+    :param bucket:
     :param tables: vector data over which to take raster statistics
     :param years: years over which to run the stats
     :param description: export name append str
@@ -80,14 +113,14 @@ def extract_gridded_data(tables, years=None, description=None, min_years=0, mask
     :return:
     """
     fc = ee.FeatureCollection(tables)
+    if features:
+        fc = fc.filter(ee.Filter.inList('BASINNUM', features))
     cmb_clip = ee.FeatureCollection(CMBRB_CLIP)
     umrb_clip = ee.FeatureCollection(UMRB_CLIP)
     corb_clip = ee.FeatureCollection(CORB_CLIP)
 
-    # fc = ee.FeatureCollection(ee.FeatureCollection(tables).filter(ee.Filter.eq('FID', 4333)))
-
     irr_coll = ee.ImageCollection(RF_ASSET)
-    coll = irr_coll.filterDate('1991-01-01', '2020-12-31').select('classification')
+    coll = irr_coll.filterDate('1987-01-01', '2021-12-31').select('classification')
     remap = coll.map(lambda img: img.lt(1))
     irr_min_yr_mask = remap.sum().gt(min_years)
     # sum = remap.sum().mask(irr_mask)
@@ -100,9 +133,6 @@ def extract_gridded_data(tables, years=None, description=None, min_years=0, mask
 
             irr = irr_coll.filterDate('{}-01-01'.format(yr), '{}-12-31'.format(yr)).select('classification').mosaic()
             irr_mask = irr_min_yr_mask.updateMask(irr.lt(1))
-
-            senay = ee.ImageCollection('projects/usgs-ssebop/et/conus/monthly/v4').filter(ee.Filter.date(s, e))
-            senay = senay.select('et_actual').sum().divide(1000.0)
 
             annual_coll = ee.ImageCollection('users/dgketchum/ssebop/cmbrb').merge(
                 ee.ImageCollection('users/hoylmanecohydro2/ssebop/cmbrb'))
@@ -122,156 +152,72 @@ def extract_gridded_data(tables, years=None, description=None, min_years=0, mask
             et_sum = ee.ImageCollection([et_cmb, et_corb, et_umrb]).mosaic()
             et = et_sum.mask(irr_mask)
 
-            tclime = ee.ImageCollection("IDAHO_EPSCOR/TERRACLIMATE").filterDate(s, e).select('pr', 'pet', 'aet')
+            tclime = ee.ImageCollection("IDAHO_EPSCOR/TERRACLIMATE").filterDate(s, e).select('aet')
             tclime_red = ee.Reducer.sum()
-            tclime_sums = tclime.select('pr', 'pet', 'aet').reduce(tclime_red)
-            ppt = tclime_sums.select('pr_sum').multiply(0.001)
-            etr = tclime_sums.select('pet_sum').multiply(0.0001)
+            tclime_sums = tclime.select('aet').reduce(tclime_red)
+            swb_aet = tclime_sums.select('aet_sum').mask(irr_mask).multiply(0.0001)
 
-            if mask_swb_to_irr:
-                swb_aet = tclime_sums.select('aet_sum').mask(irr_mask).multiply(0.0001)
-            else:
-                swb_aet = tclime_sums.select('aet_sum').multiply(0.0001)
+            ppt, etr = extract_gridmet_monthly(yr, month)
 
             irr_mask = irr_mask.reproject(crs='EPSG:5070', scale=30)
             et = et.reproject(crs='EPSG:5070', scale=30).resample('bilinear')
             ppt = ppt.reproject(crs='EPSG:5070', scale=30).resample('bilinear')
-            if mask_ppt_to_irr:
-                ppt = ppt.mask(irr_mask)
+            ppt_irr = ppt.mask(irr_mask).rename('ppt_irr')
             etr = etr.reproject(crs='EPSG:5070', scale=30).resample('bilinear')
             swb_aet = swb_aet.reproject(crs='EPSG:5070', scale=30).resample('bilinear')
 
             cc = et.subtract(swb_aet)
 
             area = ee.Image.pixelArea()
+            irr = irr_mask.multiply(area).rename('irr')
+            et = et.multiply(area).rename('et')
+            cc = cc.multiply(area).rename('cc')
+            ppt = ppt.multiply(area).rename('ppt')
+            ppt_irr = ppt_irr.multiply(area).rename('ppt_irr')
+            etr = etr.multiply(area).rename('etr')
+            swb_aet = swb_aet.multiply(area).rename('swb_aet')
 
-            if volume:
-                irr = irr_mask.multiply(area).rename('irr')
-                et = et.multiply(area).rename('et')
-                cc = cc.multiply(area).rename('cc')
-                ppt = ppt.multiply(area).rename('ppt')
-                etr = etr.multiply(area).rename('etr')
-                swb_aet = swb_aet.multiply(area).rename('aet')
+            if yr > 1986 and month in np.arange(4, 11):
+                bands = irr.addBands([et, cc, ppt, etr, swb_aet, ppt_irr])
+                select_ = ['BASINNUM', 'BASINNAME', 'irr', 'et', 'cc', 'ppt', 'etr', 'swb_aet', 'ppt_irr']
             else:
-                irr = irr_mask.multiply(area).rename('irr')
-                et = et.rename('et_ketchum')
-                senay = senay.rename('et_senay')
-                cc = cc.rename('cc')
-                ppt = ppt.rename('ppt')
-                etr = etr.rename('etr')
-                swb_aet = swb_aet.rename('aet')
+                bands = ppt.addBands([etr, ppt])
+                select_ = ['BASINNUM', 'BASINNAME', 'ppt', 'etr']
 
-            selector = ['BASINNUM', 'BASINNAME']
-            if volume:
-                bands = irr.addBands([et, cc, ppt, etr, swb_aet])
-                select_ = selector + ['irr', 'et', 'cc', 'ppt', 'etr', 'swb_aet']
-            else:
-                bands = irr.addBands([et, cc, ppt, etr, swb_aet, senay])
-                select_ = selector + ['irr', 'et', 'cc', 'ppt', 'etr', 'swb_aet', 'senay']
+            data = bands.reduceRegions(collection=fc,
+                                       reducer=ee.Reducer.sum(),
+                                       scale=30)
 
-            if volume:
-                data = bands.reduceRegions(collection=fc,
-                                           reducer=ee.Reducer.sum(),
-                                           scale=30)
-            else:
-                data = bands.reduceRegions(collection=fc,
-                                           reducer=ee.Reducer.mean(),
-                                           scale=30)
-
-            # fields = data.first().propertyNames().remove('.geo')
-            # p = data.first().getInfo()['properties']
+            if debug:
+                pt = bands.sample(region=get_geomteries()[2],
+                                  numPixels=1,
+                                  scale=30)
+                fields = pt.propertyNames().remove('.geo')
+                p = data.first().getInfo()['properties']
 
             out_desc = '{}_{}_{}'.format(description, yr, month)
             task = ee.batch.Export.table.toCloudStorage(
                 data,
                 description=out_desc,
-                bucket='wudr',
+                bucket=bucket,
                 fileNamePrefix=out_desc,
                 fileFormat='CSV',
                 selectors=select_)
-
             task.start()
             print(out_desc)
 
 
-def extract_flux_stations(shp):
-    with fiona.open(shp, 'r') as src:
-        dct = {}
-        for feat in src:
-            p = feat['properties']
-            if p['basin'] == 'umrb':
-                dct[p['site_id']] = p
-                dct[p['site_id']]['clip_feat'] = UMRB_CLIP
-                dct[p['site_id']]['geo'] = feat['geometry']['coordinates']
-            elif p['basin'] == 'cmbrb':
-                dct[p['site_id']] = p
-                dct[p['site_id']]['clip_feat'] = CMBRB_CLIP
-                dct[p['site_id']]['geo'] = feat['geometry']['coordinates']
-            else:
-                continue
-
-    et_comp = []
-    for site, props in dct.items():
-
-        if props['basin'] == 'cmbrb':
-            annual_coll = ee.ImageCollection('users/dgketchum/ssebop/cmbrb').merge(
-                ee.ImageCollection('users/hoylmanecohydro2/ssebop/cmbrb'))
-        elif props['basin'] == 'umrb':
-            annual_coll = ee.ImageCollection('projects/usgs-ssebop/et/umrb')
-        else:
-            continue
-
-        _file = '{}_monthly_data.csv'.format(props['site_id'])
-        csv = os.path.join(FLUX_DIR, _file)
-        df = read_csv(csv)
-        df = df[df['ET_corr'].notna()]
-        et_ssebop = []
-        dates = [('{}-01'.format(x[:7]), x) for x in df.date.values]
-        et_corr = [x for x in df['ET_corr'].values]
-
-        geo = ee.Geometry.Point(props['geo'][0], props['geo'][1]).buffer(3.5 * 30.0)
-
-        for et_ec, (s, e) in zip(et_corr, dates):
-            et_coll = annual_coll.filter(ee.Filter.date(s, e))
-            et = et_coll.sum().multiply(0.01)
-
-            data = et.reduceRegion(geometry=geo,
-                                   reducer=ee.Reducer.mean(),
-                                   scale=30)
-            try:
-                ee_obj = data.getInfo()
-                et_extract = ee_obj['et']
-                if et_extract is None:
-                    et_extract = np.nan
-            except (ee.EEException, KeyError):
-                et_extract = np.nan
-
-            et_ssebop.append(et_extract)
-            if not np.any(np.isnan([et_ec, et_extract])):
-                et_comp.append((et_ec, et_extract))
-
-            print('{}: {:.2f} et, {:.2f} ssebop, {}'.format(site, et_ec, et_extract, s))
-
-        df['et_ssebop'] = et_ssebop
-        _file = '{}_monthly_data_ee.csv'.format(props['site_id'])
-        csv = os.path.join(FLUX_DIR, _file)
-        df.to_csv(csv)
-
-    scatter = '/home/dgketchum/Downloads/et_comp.png'
-    et_ec = np.array([x[0] for x in et_comp])
-    et_model = np.array([x[1] for x in et_comp])
-    rmse = np.sqrt(np.mean((et_model - et_ec) ** 2))
-    lin = np.arange(0, 200)
-    plt.plot(lin, lin)
-    # m, b = np.polyfit(et_ec, et_model, 1)
-    m, b, r, p, stderr = linregress(et_ec, et_model)
-    plt.annotate('{:.2f}x + {:.2f}\n rmse: {:.2f} mm/month\n r2: {:.2f}'.format(m, b, rmse, r ** 2),
-                 xy=(0.05, 0.75), xycoords='axes fraction')
-    plt.plot(lin, m * lin + b)
-    plt.scatter(et_ec, et_model)
-    plt.ylabel('SSEBop ET')
-    plt.xlabel('Eddy Covariance')
-    plt.savefig(scatter)
+def extract_gridmet_monthly(year, month):
+    m_str, m_str_next = str(month).rjust(2, '0'), str(month + 1).rjust(2, '0')
+    if month == 12:
+        dataset = ee.ImageCollection('IDAHO_EPSCOR/GRIDMET').filterDate('{}-{}-01'.format(year, m_str),
+                                                                        '{}-{}-31'.format(year, m_str))
+    else:
+        dataset = ee.ImageCollection('IDAHO_EPSCOR/GRIDMET').filterDate('{}-{}-01'.format(year, m_str),
+                                                                        '{}-{}-01'.format(year, m_str_next))
+    pet = dataset.select('etr').sum().multiply(0.001).rename('gm_etr')
+    ppt = dataset.select('pr').sum().multiply(0.001).rename('gm_ppt')
+    return ppt, pet
 
 
 def get_landcover_info(basin, glob='none'):
@@ -302,8 +248,8 @@ def get_landcover_info(basin, glob='none'):
     # pprint(data.getInfo())
 
     for prop, var in zip(
-            ['clay', 'sand', 'loam', 'ksat', 'awc', 'nlcd', 'elevation', 'landfire_cover', 'landfire_type'],
-            [clay, sand, loam, ksat, awc, nlcd, dem, landfire_cov, landfire_type]):
+        ['clay', 'sand', 'loam', 'ksat', 'awc', 'nlcd', 'elevation', 'landfire_cover', 'landfire_type'],
+        [clay, sand, loam, ksat, awc, nlcd, dem, landfire_cov, landfire_type]):
         if 'nlcd' not in prop:
             continue
         desc = '{}_{}_{}'.format(prop, basin, glob)
@@ -355,12 +301,12 @@ def attribute_irrigation(collection, polygons, years):
 
 if __name__ == '__main__':
     is_authorized()
-
-    extract_gridded_data(DNRC_BASINS, years=[i for i in range(1991, 2021)],
-                         description='DNRC_Basins_3OCT2022', min_years=5,
-                         mask_swb_to_irr=True, volume=True, mask_ppt_to_irr=True)
-
-    # attribute_irrigation(RF_ASSET, FIELDS, years=[x for x in range(1986, 2022)])
-
-    # get_landcover_info('huc6_MT_intersect_dissolve', glob='7MAR2022')
+    # feats = [k for k, v in BASINS.items() if v[1]]
+    # export_gridded_data(DNRC_BASINS, 'wudr', years=[i for i in range(1987, 2022)],
+    #                     description='basins_30OCT2022', min_years=5, features=None)
+    region = 'users/dgketchum/boundaries/MT'
+    pts = 'users/dgketchum/hydrography/naip_pts'
+    year_ = 2017
+    desc = 'naip'
+    extract_naip(desc, pts, region, year_)
 # ========================= EOF ================================================================================
