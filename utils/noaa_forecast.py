d092718b06dcbf9f87ee8a1a53725557f070c9fd
dgketchum <dgketchum@gmail.com>
2023-05-05 12:36:52 -0600

    limit to GFS for now

--- utils/noaa_forecast.py
+++ utils/noaa_forecast.py
@@ -2,6 +2,7 @@ import os
 import pytz
 from datetime import datetime, date
 
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.dates as mdates
@@ -13,56 +14,81 @@ from herbie import Herbie
 from noaa_sdk import NOAA
 
 
-def test_nomad(met_dir, fxx_length=72):
-    now = datetime.now()
+""""
+ Get NOAA data from some different sources: gridded Global Forecast System and North American Mesoscale model
+ temperature data at long and short time scales, respectively are fetched using Herbie 
+ (https://github.com/blaylockbk/Herbie/tree/main/herbie).
+ Precipitationn data (gridded) is from the NOAA Weather Prediction Center's Quantitative Precipitation Forecast, 
+ which is an ensemble of various numerical models, using direct get request in xarray.
+ Point weather data is fetched using the NOAA-SDK (https://github.com/paulokuong/noaa).
+ 
+ 
+ See keys: 
+ 
+ NAM: 'https://www.nco.ncep.noaa.gov/pmb/products/nam/nam.t00z.conusnest.hiresf06.tm00.grib2.shtml'
+ GFS: 'https://www.nco.ncep.noaa.gov/pmb/products/gfs/gfs.t00z.pgrb2.0p25.f003.shtml'
+
+"""
+
+
+def get_gridded_forecast(met_dir, start_date='2023-05-04'):
+    """
+    Get gridded meteorological data from GFS and NAM, using NAM up to three days out, then GFS to seven days.
+    """
+    now = datetime.strptime(start_date, '%Y-%m-%d')
     mountain_tz = pytz.timezone('US/Mountain')
+    forecast_hours = list(range(24, 264, 24))
 
-    if fxx_length <= 72:
-        now = mountain_tz.localize(now)
-        now = now.astimezone(pytz.utc)
-        rounded = datetime(
-            year=now.year,
-            month=now.month,
-            day=now.day,
-            hour=(now.hour // 6) * 6,
-            minute=0,
-            second=0,
-            microsecond=0)
-    else:
-        rounded = date.today()
 
-    dt_str = rounded.strftime('%Y-%m-%d %H')
+    for fxx in forecast_hours:
+        if fxx <= 10:
+            key_csv = '../data/gfs_keys.csv'
+            model = 'nam'
+            product = None
+            now = mountain_tz.localize(now)
+            now = now.astimezone(pytz.utc)
+            rounded = datetime(
+                year=now.year,
+                month=now.month,
+                day=now.day,
+                hour=(now.hour // 6) * 6,
+                minute=0,
+                second=0,
+                microsecond=0)
+        else:
+            key_csv = '../data/gfs_keys.csv'
+            model = 'gfs'
+            product = 'pgrb2.0p25'
+            rounded = date.today()
 
-    H = Herbie(
-        dt_str,
-        model='gfs',
-        product='pgrb2.0p25',
-        fxx=240
-    )
+        dt_str = rounded.strftime('%Y-%m-%d %H')
 
-    key_url = 'https://www.nco.ncep.noaa.gov/pmb/products/nam/nam.t00z.conusnest.hiresf06.tm00.grib2.shtml'
-    keys = pd.read_html(key_url)[1]
-    inv = H.inventory()
-    match = [i for i in keys.index if i in inv.index]
-    inv.loc[match, 'desc'] = keys.loc[match, 'Description']
-    inv = inv[['search_this', 'desc', 'grib_message', 'start_byte', 'end_byte', 'range', 'reference_time', 'valid_time',
-               'variable', 'level', 'forecast_time']]
+        H = Herbie(
+            dt_str,
+            model=model,
+            product=product,
+            fxx=fxx)
 
-    ds = H.xarray('MAXVW:10 m above')
-    # ds = H.xarray('TMP:2 m above')
+        inv = H.inventory()
+        keys = pd.read_csv(key_csv)
 
-    ax = EasyMap('50m', crs=ds.herbie.crs, figsize=(8, 8)).STATES().ax
-    p = ax.pcolormesh(ds.longitude, ds.latitude, ds.unknown, transform=pc,
-                      **cm_pcp(units='mm').cmap_kwargs)
+        match = [i for i in keys.index if i in inv.index]
+        try:
+            inv.loc[match, 'desc'] = keys.loc[match, 'Description']
+        except KeyError:
+            inv.loc[match, 'desc'] = keys.loc[match, 'desc']
 
-    plt.colorbar(p, ax=ax, orientation='horizontal', pad=0.05, **cm_pcp(units='mm').cbar_kwargs)
+        inv = inv[['search_this', 'desc', 'grib_message', 'start_byte', 'end_byte',
+                   'range', 'reference_time', 'valid_time',
+                   'variable', 'level', 'forecast_time']]
 
-    ax.set_title(
-        f"{ds.model.upper()}: {H.product_description} "
-        f"({H.product})\nValid: {ds.valid_time.dt.strftime('%H:%M UTC %d %b %Y').item()}",
-        loc="left")
-    ax.set_title(ds.unknown.GRIB_name, loc="right")
-    plt.show()
+        var_search = {v['variable'].lower(): v['search_this'] for k, v in inv.iterrows()}
+
+        ds = H.xarray(var_search['tmax'])
+        slice_args = dict(latitude=slice(50, 31.5), longitude=slice(360 - 125, 360 - 100))
+        ds = ds.sel(**slice_args)
+        tmmx = H.xarray(var_search['tmax']).sel(**slice_args).tmax.values
+        tmmn = H.xarray(var_search['tmin']).sel(**slice_args).tmin.values
 
 
 def test_noaa():
@@ -92,6 +118,5 @@ if __name__ == '__main__':
 
     # test_noaa()
     met_dir_ = os.path.join(root, 'climate', 'forecast')
-    test_nomad(met_dir_, fxx_length=240)
+    get_gridded_forecast(met_dir_)
 # ========================= EOF ====================================================================
-
