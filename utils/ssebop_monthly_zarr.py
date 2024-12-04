10b87e7cd3710c65b77f72588c221511a60ba340
dgketchum <dgketchum@gmail.com>
2024-12-04 13:50:13 -0700

    minor refactor runs

--- utils/ssebop_monthly_zarr.py
+++ utils/ssebop_monthly_zarr.py
@@ -1,25 +1,23 @@
-import geopandas as gpd
-import xarray as xr
-import zarr
+import os
+
 import fsspec
+import geopandas as gpd
 import pandas as pd
-from shapely.geometry import mapping
+import xarray as xr
 
 
 def extract_zonal_stats(shapefile_path, zarr_path, endpoint_url):
-    url = "https://mghp.osn.xsede.org/cnh-bucket-1/llc4320_zarr"
-    ds = xr.open_dataset(url, engine='zarr').isel(time=0)
+    """"""
+    fs = fsspec.filesystem('s3', anon=True, endpoint_url=endpoint_url)
+    ds = xr.open_dataset(fs.get_mapper(zarr_path), engine='zarr', backend_kwargs={'consolidated': True})
+    ds['time'] = pd.to_datetime(ds['time'].values)
+    print(ds)
 
     gdf = gpd.read_file(shapefile_path)
     gdf = gdf.to_crs('epsg:4326')
 
-    fs = fsspec.filesystem('s3', anon=True, endpoint_url=endpoint_url)
-    ds = xr.open_dataset(fs.get_mapper(zarr_path), engine='zarr', backend_kwargs={'consolidated': True})
-    ds['time'] = pd.to_datetime(ds['time'].values)
-
     results = []
     for i, row in gdf.iterrows():
-        polygon_geometry = mapping(row['geometry'])
         clipped_ds = ds.sel(lat=slice(row['geometry'].bounds[1], row['geometry'].bounds[3]),
                             lon=slice(row['geometry'].bounds[0], row['geometry'].bounds[2]))
 
@@ -29,13 +27,14 @@ def extract_zonal_stats(shapefile_path, zarr_path, endpoint_url):
         results.append(time_series)
 
     df = pd.DataFrame(results).T
-    df.columns = gdf['OBJECTID'].values
     df.index = ds['time'].values
     df.to_csv('zonal_stats.csv', index_label='datetime')
 
 
 if __name__ == '__main__':
-    shapefile_path_ = '/home/ubuntu/data/IrrigationGIS/gnss/sierra_bd_merged/sierra_bd_merged_5071.shp'
+    home = os.path.expanduser('~')
+    shapefile_path_ = os.path.join(home, 'data', 'IrrigationGIS', 'gnss',
+                                   'sierra_bd_merged', 'sierra_bd_merged_5071.shp')
     zarr_path_ = 's3://mdmf/gdp/ssebopeta_monthly.zarr'
     endpoint_url_ = 'https://usgs.osn.mghpcc.org/'
 
